e97b05e376dbcbe30080fc6ed8e42647755f6ef8
iigonzoii <a.gonzalez418@gmail.com>
2021-01-09 15:46:35 -0700

    added button event

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 
 // WHEN I click the button to generate a password
+// --something happens when i click the button
 // THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
+// --after i click the button i see a prompt
 // WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
+// -- length needs to be between 8 and 128
+function generatePassword(){
+ var passwordLength = prompt("length needs to be between 8 and 128 characters")
+ console.log(passwordLength)
+}
+
+
+
+
+// WHEN confirm for character types to include in the password
 // THEN I choose lowercase, uppercase, numeric, and/or special characters
 // WHEN I answer each prompt
+
 // THEN my input should be validated and at least one character type should be selected
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
