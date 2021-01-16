512fe40672e1611832e40dfa882392fe3bd53b59
iigonzoii <a.gonzalez418@gmail.com>
2021-01-15 17:25:14 -0700

    added engineering doc in comment fashion

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// how am i storing the data?... list global variables as strings at the top
 var numbers = "0123456789";
 var specialChars = "!@#$%^&*()_+~`|}{[]:;?/-=";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
@@ -10,7 +11,7 @@ var desiredLength = 0;
 var includedCharContainer = "";
 var newPassword = "";
 
-
+// how do i prompt choices?.. create a function to prompt the choices
 function choices() {
   desiredLength = prompt("Please choose the length of your password (must be between 8 and 128 characters).")
   upperCaseResponse = confirm("do you want to include upper case letters?");
@@ -19,6 +20,9 @@ function choices() {
   lowerCaseResponse = confirm("do you want to include lower case letters?");
 }
 
+/* how do i create somewhere that stores all the different types of characters based on their selection?....
+	write a function that references our boolean variables to figure out which strings to include in our master string.....
+	we will concat the included strings based on the user input criteria. */
 function generateCharOptions(){
   if (upperCaseResponse == true)
     includedCharContainer = includedCharContainer.concat(upperCase)
@@ -29,7 +33,10 @@ function generateCharOptions(){
   if (lowerCaseResponse == true)
   includedCharContainer = includedCharContainer.concat(lowerCase)
 }
-
+/* how do we generate the password?...i will create a function to randomize the characters in our library based on the users selection
+	we write a for loop that is set to run the length of desired character selection
+	appends each character to the password string 
+ */
 function generatePassword(){
   for (i=0; i < desiredLength; i++){
     var random = Math.floor(Math.random()*includedCharContainer.length);
